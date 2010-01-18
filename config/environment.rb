83bdf1841149e53e777a23e71eeeab3112676b9c
George Guimarães <george@plataformatec.com.br>
2010-01-18 15:21:25 -0200

    installing simple_form and show_for

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   
   config.gem "rails-footnotes"
   config.gem "inherited_resources"
+  # config.gem "simple_form"
+  config.gem "show_for"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
